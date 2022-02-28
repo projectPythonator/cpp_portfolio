bea14115dd03281e22203fac9cd36c600a7bb6f8
Agis Daniels <Danjkstra@localhost.localdomain>
2022-02-28 15:47:08 -0500

    added my self a base log in a registraction ssystem in c++

---
+++ loginAndRegistrationProject/README.md
@@ -0,0 +1,4 @@
+testing the waters
+makeing a basic log in and registration system in c++
+plans to make it use encryption later, and also gui interface, also db
+so far only basic c++ is needed
---
+++ loginAndRegistrationProject/main.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool is_logged_in() {
+  string username, password, un, pw;
+  cout << "Enter a username: ";
+  cin >>  username;
+  cout << "Enter a password: ";
+  cin >> password;
+  ifstream read("./" + username + ".txt");
+  getline(read, un);
+  getline(read, pw);
+  return (un == username && password == pw);
+}
+
+bool register_user(){
+  string username, password;
+  cout << "select a username: "; 
+  cin >> username;
+  cout << "select a password: "; 
+  cin >> password;
+  ofstream file;
+  file.open("./" + username + ".txt");
+  file << username << endl << password << endl;
+  file.close();
+  return true;
+}
+
+int main(){
+  int option;
+  cout << "0: exit\n1: Register\n2 Login\nEnter your choice";
+  cin >> option;
+  while(option){
+    if (option == 1) {
+      register_user(); 
+    }else if(option == 2){
+      bool res = is_logged_in();
+      if (!res) {
+        cout << "invalid login!" << endl;
+        break;
+      } else {
+        cout << "successful login!" << endl;
+        break;
+      }
+    } else {
+      break;
+    }
+    cin >> option;
+  }
+  return 0;
+}
